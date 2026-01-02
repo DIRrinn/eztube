8b49c258e7f7ac26abccdc0f9b87dcfb4863178e
dir-rin <cosmologicalhorizon@yandex.ru>
2026-01-02 16:26:25 +0700

    добавляно лого

--- gui.py
+++ gui.py
@@ -1,5 +1,7 @@
 import sys
 from PyQt5.QtWidgets import *
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtCore import Qt
 
 
 class Window(QWidget):
@@ -9,7 +11,6 @@ class Window(QWidget):
 
     def initUI(self):
 
-        # buttons_names = ["Config", "Set Url", "Download", "Exit"]
         # Кнопатьки
         self.config_button = QPushButton('Config', self)
         self.set_url = QPushButton('Set Url', self)
@@ -21,16 +22,28 @@ class Window(QWidget):
         self.download.clicked.connect(self.on_config_clicked)
         self.exit.clicked.connect(self.on_config_clicked)
 
-        # Создаем вертикальный layout и добавляем кнопку
+        # Картинка
+        self.image = QLabel(self)
+        pixmap = QPixmap('/home/dirrinn/Images/Works/eztubelogo.png')
+        self.image.setPixmap(pixmap)
+        self.image.setScaledContents(True)
+        self.image.setFixedSize(350, 250)
+        self.image.setAlignment(Qt.AlignCenter)
+
+        # Добавляем виджеты и настраиваем
         menu_widget = QListWidget()
         layout = QVBoxLayout()
-        layout.setContentsMargins(0, 0, 0, 0)
-        layout.setSpacing(0)
+        layout.setContentsMargins(10, 10, 10, 10)
+        layout.setSpacing(2)
+
+        layout.addWidget(self.image)
         layout.addWidget(self.config_button)
         layout.addWidget(self.set_url)
         layout.addWidget(self.download)
         layout.addWidget(self.exit)
+        layout.addStretch(1)
 
+        # Настройка окна
         self.setLayout(layout)
         self.setGeometry(300, 300, 300, 300)
         self.setWindowTitle("EZtube")
@@ -39,7 +52,6 @@ class Window(QWidget):
         QMessageBox.information(self, 'Лолкек', 'Ты крутой, но тут ничего нет :(')
         
 if __name__ == '__main__':
-
     app = QApplication(sys.argv)
     window = Window()
     window.show()
