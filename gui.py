951b6403da716c1e31007f9ab5509d3a226e81fe
dir-rin <cosmologicalhorizon@yandex.ru>
2026-02-21 23:56:11 +0700

    HELL YEAH

--- gui.py
+++ gui.py
@@ -1,6 +1,6 @@
 import sys
 from PyQt5.QtWidgets import *
-from PyQt5.QtGui import QPixmap
+#from PyQt5.QtGui import QPixmap
 from PyQt5.QtCore import Qt
 
 
@@ -10,47 +10,70 @@ class Window(QWidget):
         self.initUI()
 
     def initUI(self):
-
-        # Кнопатьки
-        self.config_button = QPushButton('Config', self)
-        self.set_url = QPushButton('Set Url', self)
-        self.download = QPushButton('Download', self)
-        self.exit = QPushButton('Exit', self)
-
-        self.config_button.clicked.connect(self.on_config_clicked)
-        self.set_url.clicked.connect(self.on_config_clicked)
-        self.download.clicked.connect(self.on_config_clicked)
-        self.exit.clicked.connect(self.on_config_clicked)
-
-        # Картинка
-        self.image = QLabel(self)
-        pixmap = QPixmap('/home/dirrinn/Images/Works/eztubelogo.png')
-        self.image.setPixmap(pixmap)
-        self.image.setScaledContents(True)
-        self.image.setFixedSize(350, 250)
-        self.image.setAlignment(Qt.AlignCenter)
-
-        # Добавляем виджеты и настраиваем
-        menu_widget = QListWidget()
-        layout = QVBoxLayout()
-        layout.setContentsMargins(10, 10, 10, 10)
-        layout.setSpacing(2)
-
-        layout.addWidget(self.image)
-        layout.addWidget(self.config_button)
-        layout.addWidget(self.set_url)
-        layout.addWidget(self.download)
-        layout.addWidget(self.exit)
-        layout.addStretch(1)
-
         # Настройка окна
-        self.setLayout(layout)
-        self.setGeometry(300, 300, 300, 300)
+        self.setFixedSize(500, 400)
         self.setWindowTitle("EZtube")
 
-    def on_config_clicked(self):
-        QMessageBox.information(self, 'Лолкек', 'Ты крутой, но тут ничего нет :(')
+        # Layout
+        self.layout = QVBoxLayout()
+        self.setLayout(self.layout)
+
+        # Кнопатьки
+        enter_url_button = QPushButton('Download', self)
+
+        # URL textbox
+        textbox = QLineEdit(self)
+        textbox.setAlignment(Qt.AlignCenter)
+        textbox.setPlaceholderText("URL")
+        textbox.setFixedSize(350, 20)
+
+        self.textbox_widget = QWidget()
+        textbox_layout = QHBoxLayout()
+        textbox_layout.setSpacing(5)
+        self.textbox_widget.setLayout(textbox_layout)
+
+        textbox_layout.addWidget(textbox)
+        textbox_layout.addWidget(enter_url_button)
         
+        # video res list
+        self.options_widget = QWidget()
+        options_layout = QGridLayout()
+        options_layout.setRowStretch(4, 1)
+        self.options_widget.setLayout(options_layout)
+
+        label = QLabel("Resolution")
+        label.setMargin(0)
+
+        self.combobox_res = QComboBox(self)
+        self.combobox_res.addItems(["360p", "480p", "720p", "1080", "Highest"])
+
+        only_audio_checkbox = QCheckBox("Only audio")
+        only_audio_checkbox.stateChanged.connect(self.change_format)
+
+        self.directory = QPushButton("Select Directory")
+        self.directory.clicked.connect(self.set_directory)
+
+        options_layout.addWidget(label, 0, 0)
+        options_layout.addWidget(self.combobox_res, 1, 0)
+        options_layout.addWidget(only_audio_checkbox, 2, 0)
+        options_layout.addWidget(self.directory)
+
+        self.addWidgets()
+
+    def addWidgets(self):
+        self.layout.addWidget(self.textbox_widget)
+        self.layout.addWidget(self.options_widget)
+
+    def set_directory(self):
+        file = str(QFileDialog.getExistingDirectory(self, "Select Directory"))
+        self.directory.setText(file)
+
+    def change_format(self, state):
+        if state == Qt.Checked:
+            self.combobox_res.setEnabled(False)
+        else:
+            self.combobox_res.setEnabled(True)
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     window = Window()
